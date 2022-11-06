45efe9e1f38fc347a567109d75b715e88d183e0a
georg <georg@GORGS-COMPUTER>
2022-11-06 13:54:41 -0800

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+
+namespace RPSsimulation
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Random rnd;
+
+        Texture2D RockSprite;
+        Texture2D ScissorsSprite;
+        Texture2D PaperSprite;
+        // all the gamepeices 
+        public static List<GamePiece> GamePieces = new List<GamePiece>();
+
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // create a new random
+            rnd = new Random();
+            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // load all sprites
+            RockSprite = Content.Load<Texture2D>("Rock");
+            ScissorsSprite = Content.Load<Texture2D>("Scissors");
+            PaperSprite = Content.Load<Texture2D>("Paper");
+
+            // create gamepeices
+            CreateGamepieces(100);
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {   
+            // exit if we press esc
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // update all the gamepieces
+            foreach (GamePiece i in GamePieces)
+            {
+                i.Update();
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // clear screen
+            GraphicsDevice.Clear(Color.White);
+            // start drawing
+            _spriteBatch.Begin();            
+            // draw the gamepieces
+            foreach (GamePiece i in GamePieces)
+            {
+                i.Draw(_spriteBatch);
+            }
+            // stop drawing
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+        // function for creating gamepieces
+        public void CreateGamepieces(int amount)
+        {
+            // create amount rocks, paper, and sissors
+            for (int i = 0; i < amount; i++)
+            {
+                GamePieces.Add(new GamePiece(new Vector2(rnd.Next(0, 800), rnd.Next(0, 400)), RockSprite, ScissorsSprite, PaperSprite));
+                GamePieces.Add(new GamePiece(new Vector2(rnd.Next(0, 800), rnd.Next(0, 400)), PaperSprite, RockSprite, ScissorsSprite));
+                GamePieces.Add(new GamePiece(new Vector2(rnd.Next(0, 800), rnd.Next(0, 400)), ScissorsSprite, PaperSprite, RockSprite));
+            }
+            
+        }
+        
+    }    
+}
---
+++ GamePiece.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace RPSsimulation
+{
+    public class GamePiece
+    {
+        public Vector2 positon;
+        public Vector2 Velocity;
+        public Texture2D image;
+        public Rectangle rect;
+        public GamePiece target;
+
+        public Texture2D targetType;
+
+        public GamePiece(Vector2 pos, Texture2D sprite, Texture2D targettype, Texture2D hunter)
+        {
+            positon = pos;
+            image = sprite;
+            targetType = targettype;
+        }
+        // update method
+        public void Update()
+        { 
+            // find the neares gamepeice, move towards it, and if it's close, "infect" it
+            NearestGamePiece(targetType, positon);
+            MoveTowards(target);
+            if (DistanceTo(target) < 10)
+            {
+                target.image = image;
+                target.targetType = targetType;
+            }
+            
+        }
+        public void Draw(SpriteBatch spritebatch)
+        {
+               spritebatch.Draw(image,
+               new Rectangle((int)positon.X, (int)positon.Y, 48, 48),
+               new Rectangle(0, 0, 16, 16),
+               Color.White);
+               
+        }
+        public void NearestGamePiece(Texture2D type, Vector2 positiontocheckfrom)
+        {
+            GamePiece nearest = Game1.GamePieces[0];
+            float nearestdistance = 10000000000000000000;
+
+            foreach (GamePiece i in Game1.GamePieces)
+            {
+                float dist = DistanceTo(i);
+                if (nearestdistance > dist && i.image == type)
+                {
+                    nearestdistance = dist;
+                    nearest = i;
+                }
+            }
+            target = nearest;
+        }
+        public float DistanceTo(GamePiece target)
+        {
+            float xdif = target.positon.X - positon.X;
+            float ydif = target.positon.Y - positon.Y;
+            float distance = (float)Math.Sqrt(xdif * xdif + ydif * ydif);
+            return distance;
+            
+        }
+        public void MoveTowards(GamePiece target)
+        {
+            if (target.image == image) return;
+            if (positon.X < target.positon.X)
+            {
+                positon.X += 1;
+            }
+            else
+            {
+                positon.X -= 1;
+            }
+            if (positon.Y < target.positon.Y)
+            {
+                positon.Y += 1;
+            }
+            else
+            {
+                positon.Y -= 1;
+            }
+        }
+        public void MoveAway()
+        {
+            if (positon.X < target.positon.X)
+            {
+                positon.X += 1;
+            }
+            else
+            {
+                positon.X -= 1;
+            }
+            if (positon.Y < target.positon.Y)
+            {
+                positon.Y += 1;
+            }
+            else
+            {
+                positon.Y -= 1;
+            }
+        }
+
+    }
+}
